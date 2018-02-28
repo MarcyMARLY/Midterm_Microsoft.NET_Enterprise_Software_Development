762ac4a5424e2c1c1f84b41950a618ff318aa84f
Мариям Сисенгали <msisengali@gmail.com>
2018-02-28 12:23:02 +0600

    file managers are completed

--- Midterm/FileManipulator/OrderFileManipulator.cs
+++ Midterm/FileManipulator/OrderFileManipulator.cs
@@ -1,11 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Midterm.Models;
 
 namespace Midterm.FileManipulator
 {
-    public class OrderFileManipulator
+    public class OrderFileManipulator : IFileManipulator<Models.Order>
     {
+        public string Path { get; set; }
+        private List<Order> OrderCollection;
+
+        public void WriteToFile(Order t)
+        {
+            string OrderString = t.route + ";" + t.category + ";" + t.amount + ";" + t.status + ";" + t.date + ";";
+            using (StreamWriter sw = File.AppendText(Path))
+            {
+                sw.WriteLine(OrderString);
+            }
+        }
+
+        public Order ConvertItem(string item)
+        {
+            var itemList = item.Split(';');
+            return new Order
+            {
+                route = itemList[0],
+                category = itemList[1],
+                amount = Convert.ToInt32(itemList[2]),
+                status = Convert.ToInt32(itemList[3]),
+                date = Convert.ToDateTime(itemList[4])
+            };
+        }
+
+        public List<Order> GetCollection()
+        {
+            if (OrderCollection == null)
+            {
+                var data = File.ReadAllLines(Path);
+                OrderCollection = data
+                    .Select(x => ConvertItem(x))
+                    .ToList();
+            }
+            return OrderCollection;
+        }
     }
 }
--- Midterm/FileManipulator/RouteFileManipulator.cs
+++ Midterm/FileManipulator/RouteFileManipulator.cs
@@ -1,11 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Midterm.Models;
 
 namespace Midterm.FileManipulator
 {
-    public class RouteFileManipulator
+    public class RouteFileManipulator : IFileManipulator<Models.Route>
     {
+        public string Path { get; set; }
+        private List<Route> RouteCollection;
+
+        public Route ConvertItem(string item)
+        {
+            var itemList = item.Split(";");
+            return new Route
+            {
+                routeName = itemList[0],
+                price = Convert.ToInt32(itemList[1]),
+                duration = Convert.ToInt32(itemList[2])
+            };
+        }
+
+        public List<Route> GetCollection()
+        {
+            var data = File.ReadAllLines(Path);
+            if (RouteCollection == null)
+            {
+                RouteCollection = data
+                    .Select(x => ConvertItem(x))
+                    .ToList();
+            }
+            return RouteCollection;
+
+        }
+
+        public void WriteToFile(Route t)
+        {
+            string RouteString = t.routeName + ";" + t.price + ";" + t.duration + ";";
+            using (StreamWriter sw = File.AppendText(Path))
+            {
+                sw.WriteLine(RouteString);
+            }
+        }
     }
 }
