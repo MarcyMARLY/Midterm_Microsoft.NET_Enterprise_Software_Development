50612c6cdde030896fcfd003f39301d39781f518
Мариям Сисенгали <msisengali@gmail.com>
2018-02-28 11:38:19 +0600

    initial changes are added

---
+++ Midterm/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Midterm.Models
+{
+    public class Category
+    {
+        public string id { get; set; }
+        public string ProductName {get;set;}
+        public int PricePerKg { get; set; }
+        public int TotalAmount { get; set; }
+
+    }
+}
---
+++ Midterm/Models/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Midterm.Models
+{
+    public enum Status
+    {
+        NotOnTime,
+        Delivered,
+        Canceled
+
+    }
+    public class Order
+    {
+        public Route route { get; set; }
+        public Category category { get; set; }
+        public int amount { get; set; }
+
+    }
+}
